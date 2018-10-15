522246b0a984a6b487384a70ff8cceff4ccccc3a
Benjamin Segall <bentekkie@gmail.com>
2018-10-15 13:30:10 -0400

    Update firebase-key.js

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
-  "apiKey": '', 
-  "authDomain": '', 
-  "databaseURL": '' 
+  "apiKey": 'AIzaSyDR9wmquLH0KZQ2rQu7czbgZWLS2fQGYHI', 
+  "authDomain": 'table-champ-1cc46.firebaseapp.com', 
+  "databaseURL": 'https://table-champ-1cc46.firebaseio.com' 
 };
